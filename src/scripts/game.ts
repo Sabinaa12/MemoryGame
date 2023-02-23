17601ff767f04e27a9596ad315bcbe0368915429
Purcarea Sabina <sabina.purcarea@reevotech.com>
2023-02-23 17:57:08 +0200

    changed the square color at event click with specific color

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -5,20 +5,35 @@ export class Game extends PIXI.Container {
     private _square: Squares;
     private _app: PIXI.Application;
     private _tableSquares: Array<Squares>;
-    private _colorsArray: any[] =[];
+    private _colorsArray: any[] = [];
+    private _colorsUsedArray: any[] = [];
+
     private _squareNumber: number = 12;
+
 
     constructor(app: PIXI.Application) {
         super();
         this._app = app;
-        this._tableSquares = new Array<Squares>();
         this.generateColor();
+
+        this._tableSquares = new Array<Squares>();
         this.createTableGame();
-        
+
     }
 
-    private showSquare(id: number): Squares {
-        this._square = new Squares(id, this._colorsArray[Math.floor(Math.random() * 12)]);
+    private showSquare(id: number): any {
+        let randomColor = (this._colorsArray[Math.floor(Math.random() * this._squareNumber - 1) + id]);
+        if (this._colorsUsedArray[this._colorsArray.indexOf(randomColor)] == false) {
+            this._square = new Squares(id, randomColor);
+
+            this._colorsUsedArray[this._colorsArray.indexOf(randomColor)] = true;
+        } else {
+            let colorFound = this._colorsUsedArray.indexOf(this._colorsUsedArray.find(element => element == false));
+            this._square = new Squares(id, this._colorsArray[colorFound]);
+            this._colorsUsedArray[colorFound] = true;
+
+        }
+
         return this._square;
     }
 
@@ -27,11 +42,9 @@ export class Game extends PIXI.Container {
         let yOffset: number = 0;
         for (let i = 0; i < this._squareNumber; i++) {
             let a = this.showSquare(i);
-            this._square._clickOnSquareHandler = (id:number,color:number) => {
-                 console.log("Click", id);
-                 color =this._colorsArray[0];
-                 console.log("Click", color);
-      
+
+            this._square._clickOnSquareHandler = (id: number) => {
+                console.log("Click", id);
             };
             this._tableSquares.push(a)
             this._tableSquares[i].name = i.toString();
@@ -57,17 +70,17 @@ export class Game extends PIXI.Container {
         }
 
         this._app.stage.addChild(...this._tableSquares);
-     
+
 
 
     }
 
     private generateColor(): void {
-        for(let i=0; i < this._squareNumber / 2; i++){
-            this._colorsArray[i] = this._colorsArray[this._squareNumber-1-i] ='0x' + Math.floor(Math.random()*16777215).toString(16);
-           
+        for (let i = 0; i < this._squareNumber / 2; i++) {
+            this._colorsArray[i] = this._colorsArray[this._squareNumber - 1 - i] = '0x' + Math.floor(Math.random() * 16777215).toString(16);
+            this._colorsUsedArray[i] = this._colorsUsedArray[this._squareNumber - 1 - i] = false;
         }
-        console.log("0x",...this._colorsArray);
+        console.log("0x", ...this._colorsArray);
     }
 
 }
--- src/scripts/squares.ts
+++ src/scripts/squares.ts
@@ -6,16 +6,14 @@ export class Squares extends PIXI.Graphics {
     private _id: number;
     private _color: number;
 
-    constructor(id: number,color:number) {
+    constructor(id: number, color: number) {
         super();
         this._id = id;
         this._color = color;
-        // console.log("!", this._id)
         this.createSquare(0xff0000);
         this.interactive = true;
         this.buttonMode = true;
         this.onClick();
-
     }
 
     private createSquare(color: any): void {
@@ -23,15 +21,13 @@ export class Squares extends PIXI.Graphics {
         this.drawRect(0, 0, 100, 100);
     }
 
-    private changecolor(color:number): void {
-        this.createSquare(color);
-    }
-    
+
     private onClick(): void {
         this.on('pointerdown', () => {
             this._clickOnSquareHandler(this._id);
             console.log("Click1", this._color);
-            this.changecolor(this._color);
+            this.createSquare(this._color);
+            this.interactive = false;
         });
     }
 }
